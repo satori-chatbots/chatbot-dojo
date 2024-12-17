2abb45d3406452c2816d4f8e1c29567573a6ad00
LittleHaku <ivandelhorno@gmail.com>
2024-12-17 13:38:16 +0100

    la base de datos se actualiza con los archivos que se añadan manualmente a la carpeta uploads, aunque sea añaden o se borren sin pasar por la API

--- backend/tester/api.py
+++ backend/tester/api.py
@@ -4,8 +4,10 @@ from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 from .models import TestCase, TestFile
 from .serializers import TestCaseSerializer, TestFileSerializer
+import os
 
 class TestCaseViewSet(viewsets.ModelViewSet):
     queryset = TestCase.objects.all()
@@ -14,6 +16,46 @@ class TestCaseViewSet(viewsets.ModelViewSet):
 class TestFileViewSet(viewsets.ModelViewSet):
     queryset = TestFile.objects.all()
     serializer_class = TestFileSerializer
+
+    # Own implementation because now if we delete a file, the row in the DB still exists
+    def list(self, request, *args, **kwargs):
+        """Return a list of all the YAML files uploaded, if one is missing, delete the row in the DB.
+        If a new file is found in the directory, add it to the DB.
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: Serialized data of the files.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Check if a file is missing, if so, delete the row in the DB
+        for file in queryset:
+            if not os.path.exists(file.file.path):
+                file.delete()
+
+        # Check for new files in the directory and add them to the DB
+        directory_path = os.path.join(settings.MEDIA_ROOT, 'user-yaml')
+        for filename in os.listdir(directory_path):
+            file_path = os.path.join(directory_path, filename)
+            if not queryset.filter(file__icontains=filename).exists():
+                new_file = TestFile(file=file_path)
+                new_file.save()
+
+
+        # Repeat the query after possible deletions
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Paginate the queryset if needed
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     # Allow bulk deletion of files without creating a new view
     @action(detail=False, methods=['delete'], url_path='delete-bulk')
@@ -24,6 +66,12 @@ class TestFileViewSet(viewsets.ModelViewSet):
         {
             "ids": [1, 2, 3]
         }
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: Response with the number of files deleted or an error message.
         """
         ids = request.data.get('ids', [])
         if not ids:
@@ -46,6 +94,12 @@ class TestFileViewSet(viewsets.ModelViewSet):
         {
             "file": [file1, file2, ...]
         }
+
+        Args:
+            request (Request): The request object.
+
+        Returns:
+            Response: If successful, returns the serialized data of the uploaded files.
         """
         files = request.FILES.getlist('file')
         if not files:
