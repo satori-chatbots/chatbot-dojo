7497168427ea2fd90373e5c5ba63439f2f62e764
LittleHaku <ivandelhorno@gmail.com>
2024-12-18 14:26:06 +0100

    separado en views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,11 @@
 import React from 'react';
-import UserProfileList from './components/UserProfileList';
-import UserProfileUpload from './components/UserProfileUpload';
-import useFetchFiles from './hooks/useFetchFiles';
-import useFileHandlers from './hooks/userFileHandlers';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './views/Home';
+import Dashboard from './views/Dashboard';
 
 function App() {
-    const { files, loading, error, reload } = useFetchFiles();
-    const {
-        selectedFiles,
-        toggleSelect,
-        handleDelete,
-        handleExecuteTest,
-    } = useFileHandlers(reload);
-
-    if (loading) {
-        return <p>Loading files...</p>;
-    }
-
-    if (error) {
-        return <p>Error fetching files: {error.message}</p>;
-    }
-
     return (
-        <div>
-            <UserProfileList
-                files={files}
-                selectedFiles={selectedFiles}
-                toggleSelect={toggleSelect}
-                handleDelete={handleDelete}
-                handleExecuteTest={handleExecuteTest}
-            />
-            <UserProfileUpload onUpload={reload} />
-        </div>
+        <Home />
     );
 }
 
---
+++ frontend/src/views/Home.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import UserProfileList from '../components/UserProfileList';
+import UserProfileUpload from '../components/UserProfileUpload';
+import useFetchFiles from '../hooks/useFetchFiles';
+import useFileHandlers from '../hooks/userFileHandlers';
+
+function Home() {
+    const { files, loading, error, reload } = useFetchFiles();
+    const {
+        selectedFiles,
+        toggleSelect,
+        handleDelete,
+        handleExecuteTest,
+    } = useFileHandlers(reload);
+
+    if (loading) {
+        return <p>Loading files...</p>;
+    }
+
+    if (error) {
+        return <p>Error fetching files: {error.message}</p>;
+    }
+
+    return (
+        <div>
+            <UserProfileList
+                files={files}
+                selectedFiles={selectedFiles}
+                toggleSelect={toggleSelect}
+                handleDelete={handleDelete}
+                handleExecuteTest={handleExecuteTest}
+            />
+            <UserProfileUpload onUpload={reload} />
+        </div>
+    );
+}
+
+export default Home;
