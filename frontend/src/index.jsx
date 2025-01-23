04fe4521a6d96a6e794a718173b00d7bdaae45a1
LittleHaku <ivandelhorno@gmail.com>
2025-01-23 22:35:02 +0100

    Dark Modo and Navbar

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,16 +4,18 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { HeroUIProvider } from "@heroui/react"
-
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 createRoot(document.getElementById('root')).render(
+
     <HeroUIProvider>
+        <NextThemesProvider attribute="class" defaultTheme="light">
+            <main className="text-foreground bg-background">
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
 
-        <main className="light text-foreground bg-background">
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-
-        </main>
+            </main>
+        </NextThemesProvider>
     </HeroUIProvider>
 );
