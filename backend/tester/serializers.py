b3b779b686a770c0edd6e0fc25dbfa2614519992
LittleHaku <ivandelhorno@gmail.com>
2025-02-06 13:47:45 +0100

    Add RegisterSerializer for user registration with password handling

--- backend/tester/serializers.py
+++ backend/tester/serializers.py
@@ -9,6 +9,21 @@ from .models import (
     ProfileReport,
     Conversation,
 )
+from django.contrib.auth import get_user_model
+
+# Get the latest version of the user model
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            return user
 
 
 class ConversationSerializer(serializers.ModelSerializer):
