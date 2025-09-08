b0bd0c948597c43d971f8aeb6f74dc496e0f3380
LittleHaku <ivandelhorno@gmail.com>
2025-09-08 11:33:39 +0200

    typo...

--- frontend/src/api/profile-report-api.js
+++ frontend/src/api/profile-report-api.js
@@ -2,10 +2,10 @@ import API_BASE_URL, { ENDPOINTS } from "./config";
 
 export const fetchProfileReportByGlobalReportId = (globalReportId) => {
   return fetch(
-    `${API_BASE_URL}${ENDPOINTS.PROFILERPORTS}?global_report_id=${globalReportId}`,
-  ).then((response) => {
+    `${API_BASE_URL}${ENDPOINTS.PROFILE_REPORTS}?global_report_id=${globalReportId}`,
+  ).then(async (response) => {
     if (!response.ok) {
-      const errorData = response.json();
+      const errorData = await response.json();
       throw new Error(JSON.stringify(errorData));
     }
     return response.json();
